00f04aa938e45a3fdf3d947ab2243de11b83b569
Finn <finn1999.duehrkop@gmail.com>
2023-12-17 14:40:06 +0100

    Initial Commit

---
+++ source/SlimEndpoints/IEndpoint.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace SlimEndpoints;
+
+public interface IEndpoint
+{
+    public static abstract string EndpointName { get; }
+    public static abstract RouteHandlerBuilder ConfigureEndpoint(IEndpointRouteBuilder builder, [RouteTemplate] string route);
+}
---
+++ source/SlimEndpoints/SlimEndpointsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace SlimEndpoints;
+
+public static class SlimEndpointsExtensions
+{
+    public static IEndpointRouteBuilder Map<TEndpoint>(this IEndpointRouteBuilder builder, [RouteTemplate] string route = "/") where TEndpoint : IEndpoint
+    {
+        TEndpoint.ConfigureEndpoint(builder, route)
+            .WithName(TEndpoint.EndpointName);
+
+        return builder;
+    }
+
+    public static RouteHandlerBuilder ProducesInternalServerError(this RouteHandlerBuilder builder, string? contentType = null) =>
+        builder.ProducesProblem((int)HttpStatusCode.InternalServerError, contentType);
+
+    public static RouteHandlerBuilder ProducesNotFound(this RouteHandlerBuilder builder, string? contentType = null) =>
+        builder.ProducesProblem((int)HttpStatusCode.NotFound, contentType);
+}
