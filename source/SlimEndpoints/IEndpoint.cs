a69ef3367eb1bc2d6b8038f073b87b5d7b807ecc
Finn <finn1999.duehrkop@gmail.com>
2023-12-17 15:10:53 +0100

    add documentation

--- source/SlimEndpoints/IEndpoint.cs
+++ source/SlimEndpoints/IEndpoint.cs
@@ -4,8 +4,21 @@ using Microsoft.AspNetCore.Routing;
 
 namespace SlimEndpoints;
 
+/// <summary>
+/// The base interface for all endpoints.
+/// </summary>
 public interface IEndpoint
 {
+    /// <summary>
+    /// The name of the endpoint. This is used to generate the OpenAPI documentation.
+    /// </summary>
     public static abstract string EndpointName { get; }
+
+    /// <summary>
+    /// Configures the endpoint on the given <paramref name="builder"/>. This method is called by the <see cref="SlimEndpointsExtensions.Map{TEndpoint}"/> method.
+    /// </summary>
+    /// <param name="builder">The <see cref="IEndpointRouteBuilder"/> to configure the endpoint on.</param>
+    /// <param name="route">The route the endpoint is reachable at.</param>
+    /// <returns>The <see cref="RouteHandlerBuilder"/> to allow fluent chaining.</returns>
     public static abstract RouteHandlerBuilder ConfigureEndpoint(IEndpointRouteBuilder builder, [RouteTemplate] string route);
 }
--- source/SlimEndpoints/SlimEndpointsExtensions.cs
+++ source/SlimEndpoints/SlimEndpointsExtensions.cs
@@ -1,13 +1,25 @@
 ﻿using System.Net;
+using System.Net.Mime;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 
 namespace SlimEndpoints;
 
+/// <summary>
+/// Extension methods for <see cref="IEndpointRouteBuilder"/> facilitating the configuration of endpoints.
+/// </summary>
 public static class SlimEndpointsExtensions
 {
+    /// <summary>
+    /// Maps the given <typeparamref name="TEndpoint"/> to the given <paramref name="builder"/> at the given <paramref name="route"/>.
+    /// </summary>
+    /// <param name="builder">The <see cref="IEndpointRouteBuilder"/> to configure the endpoint on.</param>
+    /// <param name="route">The route the endpoint is reachable at.</param>
+    /// <typeparam name="TEndpoint">The Endpoint to configure.</typeparam>
+    /// <returns>The <see cref="IEndpointRouteBuilder"/> to allow fluent chaining.</returns>
     public static IEndpointRouteBuilder Map<TEndpoint>(this IEndpointRouteBuilder builder, [RouteTemplate] string route = "/") where TEndpoint : IEndpoint
     {
         TEndpoint.ConfigureEndpoint(builder, route)
@@ -16,9 +28,21 @@ public static class SlimEndpointsExtensions
         return builder;
     }
 
-    public static RouteHandlerBuilder ProducesInternalServerError(this RouteHandlerBuilder builder, string? contentType = null) =>
+    /// <summary>
+    /// Configures endpoint metadata to produce <see cref="ProblemDetails"/> for <see cref="HttpStatusCode.InternalServerError"/>. Useful for OpenAPI documentation.
+    /// </summary>
+    /// <param name="builder">The <see cref="RouteHandlerBuilder"/> to configure the endpoint on.</param>
+    /// <param name="contentType">The content type of the <see cref="ProblemDetails"/>. Defaults to <see cref="MediaTypeNames.Application.ProblemJson"/>.</param>
+    /// <returns>The <see cref="RouteHandlerBuilder"/> to allow fluent chaining.</returns>
+    public static RouteHandlerBuilder ProducesInternalServerError(this RouteHandlerBuilder builder, string? contentType = MediaTypeNames.Application.ProblemJson) =>
         builder.ProducesProblem((int)HttpStatusCode.InternalServerError, contentType);
 
-    public static RouteHandlerBuilder ProducesNotFound(this RouteHandlerBuilder builder, string? contentType = null) =>
+    /// <summary>
+    /// Configures endpoint metadata to produce <see cref="ProblemDetails"/> for <see cref="HttpStatusCode.NotFound"/>. Useful for OpenAPI documentation.
+    /// </summary>
+    /// <param name="builder">The <see cref="RouteHandlerBuilder"/> to configure the endpoint on.</param>
+    /// <param name="contentType">The content type of the <see cref="ProblemDetails"/>. Defaults to <see cref="MediaTypeNames.Application.ProblemJson"/>.</param>
+    /// <returns>The <see cref="RouteHandlerBuilder"/> to allow fluent chaining.</returns>
+    public static RouteHandlerBuilder ProducesNotFound(this RouteHandlerBuilder builder, string? contentType = MediaTypeNames.Application.ProblemJson) =>
         builder.ProducesProblem((int)HttpStatusCode.NotFound, contentType);
 }
